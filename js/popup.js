0271d29871009c683cce82e25675ec0e0f2bfe06
su <liang-shan@outlook.com>
2022-01-08 20:01:52 +0800

    extension add words
    popup language select

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,11 @@
-let language = document.getElementById("language");
+let languageEle = document.getElementById("language");
 
-// chrome.storage.sync.get(['language'], (target) => {
-//     console.log("get", target)
-//     if (target) {
-//         language.value = target
-//     }
-// })
-//
-//
-// language.addEventListener("change", (e) => {
-//     console.log("set", language.value)
-//     chrome.storage.sync.set({language: language.value})
-// })
+chrome.storage.sync.get("language", ({language}) => {
+    if (language) {
+        languageEle.value = language
+    }
+});
+
+languageEle.addEventListener("change", () => {
+    chrome.storage.sync.set({language: languageEle.value})
+})
